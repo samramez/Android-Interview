0053b8ae0f34eded2bebe58eb8a348e450ff2996
Sam Ramezanli <samramezz@gmail.com>
2015-02-23 00:39:11 -0500

    Fade Animation works perfectly
    Special thanks to:
    http://stackoverflow.com/a/12203794/1366145

--- app/src/main/java/com/apppartner/androidprogrammertest/AnimationActivity.java
+++ app/src/main/java/com/apppartner/androidprogrammertest/AnimationActivity.java
@@ -1,9 +1,5 @@
 package com.apppartner.androidprogrammertest;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -36,27 +32,9 @@ public class AnimationActivity extends ActionBarActivity {
         fadeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                appPartnerImageView.startAnimation(animationFadeOut);
-//
-//                appPartnerImageView.startAnimation(animationFadeIn);
+                appPartnerImageView.startAnimation(animationFadeOut);
 
-                ObjectAnimator fadeOut = ObjectAnimator.ofFloat(appPartnerImageView, "alpha",  1f, 0.01f);
-                fadeOut.setDuration(2000);
-                ObjectAnimator fadeIn = ObjectAnimator.ofFloat(appPartnerImageView, "alpha", 0.01f, 1f);
-                fadeIn.setDuration(2000);
-
-                final AnimatorSet mAnimationSet = new AnimatorSet();
-
-                mAnimationSet.play(fadeIn).after(fadeOut);
-
-                mAnimationSet.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        super.onAnimationEnd(animation);
-                        mAnimationSet.start();
-                    }
-                });
-                mAnimationSet.start();
+                //appPartnerImageView.startAnimation(animationFadeIn);
             }
         });
 
