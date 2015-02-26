ad639346cb82e651df663097bc6ff14db6284dca
Sam Ramezanli <samramezz@gmail.com>
2015-02-25 23:48:01 -0500

    Chat works fine (ActionBarActivity is left)

--- app/src/main/java/com/apppartner/androidprogrammertest/adapters/ChatsArrayAdapter.java
+++ app/src/main/java/com/apppartner/androidprogrammertest/adapters/ChatsArrayAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.apppartner.androidprogrammertest.R;
@@ -24,6 +25,7 @@ public class ChatsArrayAdapter extends ArrayAdapter<ChatData>
         super(context, 0, objects);
     }
 
+    @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
         ChatCell chatCell = new ChatCell();
@@ -33,12 +35,17 @@ public class ChatsArrayAdapter extends ArrayAdapter<ChatData>
 
         chatCell.usernameTextView = (TextView) convertView.findViewById(R.id.usernameTextView);
         chatCell.messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
+        chatCell.userImageView = (ImageView) convertView.findViewById(R.id.userImageView) ;
 
         ChatData chatData = getItem(position);
 
+        // We want to specify the owner of the picture
+        String owner = chatData.username;
+        int picOwner = whosePicture(owner);
+
         chatCell.usernameTextView.setText(chatData.username);
         chatCell.messageTextView.setText(chatData.message);
-
+        chatCell.userImageView.setBackgroundResource(picOwner);
         return convertView;
     }
 
@@ -46,5 +53,27 @@ public class ChatsArrayAdapter extends ArrayAdapter<ChatData>
     {
         TextView usernameTextView;
         TextView messageTextView;
+        ImageView userImageView;
     }
+
+    private int whosePicture(String userTextView){
+
+        int[] imageIds = {
+                R.drawable.justin,
+                R.drawable.drew,
+                R.drawable.greg,
+                R.drawable.derrick
+        };
+
+        if( userTextView.contains("Justin") )
+            return imageIds[0];
+        else if(userTextView.contains("Drew") )
+            return imageIds[1];
+        else if( userTextView.contains("Greg") )
+            return imageIds[2];
+        else
+            return imageIds[3];
+    }
+
+
 }
